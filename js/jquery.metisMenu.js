097fb63336291c7e93e59984534557497c0beb58
tchiba0103 <tchiba0103@gmail.com>
2025-07-02 01:18:27 +0900

    add rest of all files and folders

---
+++ js/jquery.metisMenu.js
@@ -0,0 +1,45 @@
+;(function ($, window, document, undefined) {
+
+    var pluginName = "metisMenu",
+        defaults = {
+            toggle: true
+        };
+        
+    function Plugin(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Plugin.prototype = {
+        init: function () {
+
+            var $this = $(this.element),
+                $toggle = this.settings.toggle;
+
+            $this.find('li.active').has('ul').children('ul').addClass('collapse in');
+            $this.find('li').not('.active').has('ul').children('ul').addClass('collapse');
+
+            $this.find('li').has('ul').children('a').on('click', function (e) {
+                e.preventDefault();
+
+                if ($(this).parent('li').hasClass('active')) {
+                    $(this).parent('li').removeClass('active').children('ul.in').collapse('hide');
+                }else{
+                	$(this).parent('li').addClass('active').children('ul').collapse('toggle');
+                }
+            });
+        }
+    };
+
+    $.fn[ pluginName ] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
